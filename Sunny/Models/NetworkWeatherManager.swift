97968c94a6f947c47ba8c7c6e749155e883efcb7
Evgeni Meleshin <evgenimeleshin@icloud.com>
2022-10-09 15:46:33 +0300

    Initial Commit

---
+++ Sunny/Models/NetworkWeatherManager.swift
@@ -0,0 +1,59 @@
+//
+//  NetworkWeatherManager.swift
+//  Sunny
+//
+//  Created by Evgeni Meleshin on 09.10.2022.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class NetworkWeatherManager {
+    
+    enum RequestType {
+        case cityName(city: String)
+        case coordinate(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
+    }
+    
+    var onComplition: ((CurrentWeather) -> Void)?
+    
+    func fetchCurrentWeather(forRequestType requestType: RequestType) {
+        var urlString = ""
+        switch requestType {
+        case .cityName(let city): urlString =
+            "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=metric"
+        case .coordinate(let latitude, let longitude): urlString =
+            "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric"
+        }
+        performRequest(withURLString: urlString)
+    }
+    
+    fileprivate func performRequest(withURLString urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) {data, response, error in
+            if let data = data {
+                if let currentWeather = self.parseJSON(withData: data) {
+                    self.onComplition?(currentWeather)
+                    //self.delegate?.updateInterface(self, with: currentWeather)
+                    
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    fileprivate func parseJSON(withData data: Data) -> CurrentWeather? {
+        let decoder = JSONDecoder()
+        do {
+            let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
+            guard let currentWeather = CurrentWeather(currentWeatherData: currentWeatherData) else { return nil }
+            return currentWeather
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        return nil
+    }
+    
+}
---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constanrs.swift
+//  Sunny
+//
+//  Created by Evgeni Meleshin on 09.10.2022.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "da698ffa43e83568fc4c83bdd1d6701d"
